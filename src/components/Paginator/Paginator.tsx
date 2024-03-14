ba6367851f22e5c354a3ab4aa657243924217a41
Kuba <339630@uwr.edu.pl>
2024-03-14 14:46:11 +0100

    better pagination

--- src/components/Paginator/Paginator.tsx
+++ src/components/Paginator/Paginator.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface IProps {
   page: number;
   setPage: (page: number) => void;
@@ -13,6 +15,9 @@ export default function ({
   totalVisibleItems,
   setItemsPerPage,
 }: IProps) {
+  const [formItemsPerPage, setFormItemsPerPage] = useState<string>(
+    itemsPerPage.toString()
+  );
   return (
     <div className="flex flex-col items-center">
       {/* <!-- Help text --> */}
@@ -80,15 +85,31 @@ export default function ({
           </svg>
         </button>
       </div>
-      <input
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        type="number"
-        value={itemsPerPage}
-        onChange={(e) => {
+      <form
+        className="flex mt-2"
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (!Number.isNaN(formItemsPerPage) && Number(formItemsPerPage) < 1) {
+            setFormItemsPerPage(itemsPerPage.toString());
+            return;
+          }
           setPage(1);
-          setItemsPerPage(Number(e.target.value));
+          setItemsPerPage(+formItemsPerPage);
         }}
-      />
+      >
+        <input
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mr-2"
+          type="number"
+          value={formItemsPerPage}
+          onChange={(e) => setFormItemsPerPage(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          OK
+        </button>
+      </form>
     </div>
   );
 }
