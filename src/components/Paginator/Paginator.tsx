c3dbfcd4d78f7e6d6bf4a01e49bd2a3896e02e04
Kuba <339630@uwr.edu.pl>
2024-04-10 13:01:09 +0200

    add "Items per page" caption

--- src/components/Paginator/Paginator.tsx
+++ src/components/Paginator/Paginator.tsx
@@ -86,7 +86,7 @@ export default function ({
         </button>
       </div>
       <form
-        className="flex mt-2"
+        className="flex mt-2 items-center"
         onSubmit={(e) => {
           e.preventDefault();
           if (!Number.isNaN(formItemsPerPage) && Number(formItemsPerPage) < 1) {
@@ -97,8 +97,11 @@ export default function ({
           setItemsPerPage(+formItemsPerPage);
         }}
       >
+        <label className="text-sm text-gray-700 dark:text-gray-400 me-2">
+          Items per page:
+        </label>
         <input
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mr-2"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mr-2 w-24"
           type="number"
           value={formItemsPerPage}
           onChange={(e) => setFormItemsPerPage(e.target.value)}
